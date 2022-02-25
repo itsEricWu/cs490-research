9467978a9fb214ad522495e708e7988196b875c4
luwh2000 <luwh2000@gmail.com>
2022-02-24 21:49:48 -0500

    image generated

--- test.py
+++ test.py
@@ -2,27 +2,13 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import pickle
+from preprocess import Preprocess
 
-def generate_v_matrix(num_condition=10, num_matrix=10, identity=True):
-    np.random.seed(0)
-    a = np.random.normal(0, 1, size=(3, 3))  # random matrix
-    print(a)
-    eps_list = np.logspace(0, 3, num_condition)
-    V_list = []
-    condition_list = []
-    for eps in eps_list:
-        for i in range(0, num_matrix):
-            C = eps
-            u, s, v = np.linalg.svd(a, full_matrices=True) #svd
-            s = s[0] * (1 - ((C - 1) / C) * (s[0] - s) / (s[0] - s[-1])) #linear stretch
-            s = np.diag(s)
-            V = u @ s @ v
-            V_list.append(V)
-            condition_list.append(eps)
-    if identity:
-        V_list.append(np.identity(3))
-        condition_list.append(-1)
-    return V_list, condition_list
 
-v, c = generate_v_matrix(10, 1, True)
-print(v)
+v, c = Preprocess.generate_constant_v_matrix(10, 1, True)
+epsilons = [0, 0.05, 0.1, 0.15, 0.2]
+for i, k in zip(v, c):
+    for j in epsilons:
+        x_new = Preprocess.preprocess_image("Test/0/10.ppm", i, j)
+        filename = "generated/jpgimages/10_" + str(k) + "_" + str(j)
+        Preprocess.show_np_array_as_jpg(x_new, filename)
