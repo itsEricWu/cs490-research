2d59c033174f64c394551b3676b6141534ce83d5
luwh2000 <luwh2000@gmail.com>
2022-02-25 17:46:31 -0500

    fixed y new and channel issue

--- image_generate.py
+++ image_generate.py
@@ -9,6 +9,7 @@ v, c = Preprocess.generate_constant_v_matrix(10, 1, True)
 epsilons = [0, 0.05, 0.1, 0.15, 0.2]
 for i, k in zip(v, c):
     for j in epsilons:
-        x_new = Preprocess.preprocess_image("Test/0/10.ppm", i, j)
+        x_new = Preprocess.preprocess_image("Test/0/32.ppm", i, j)
         filename = "generated/jpgimages/10_" + str(k) + "_" + str(j)
+        # Preprocess.show_np_array_as_jpg(x_new[:, :, ::-1], filename)
         Preprocess.show_np_array_as_jpg(x_new, filename)
--- preprocess.py
+++ preprocess.py
@@ -26,7 +26,7 @@ class Preprocess():
         # some scenes are not possible at all.
 
         img = cv2.imread(img_filename)  # numpy array
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         # Captured Image - original #
         y = Preprocess.im2double(img)  # Convert to normalized floating point
@@ -40,6 +40,13 @@ class Preprocess():
         ylong[:, 1] = y2.flatten()
         y3 = y[:, :, 2]  # (480,640)
         ylong[:, 2] = y3.flatten()
+
+        # y1 = y[:, :, 2]  # (480,640)
+        # ylong[:, 0] = y1.flatten()
+        # y2 = y[:, :, 1]  # (480,640)
+        # ylong[:, 1] = y2.flatten()
+        # y3 = y[:, :, 0]  # (480,640)
+        # ylong[:, 2] = y3.flatten()
 
         xlong = np.transpose(np.matmul(np.linalg.pinv(v), np.transpose(ylong)))
         xlong[xlong > 1] = 1
@@ -61,20 +68,22 @@ class Preprocess():
         x_new[:, :, 1] = xlong_new[:, 1].reshape(ss[0], ss[1])
         x_new[:, :, 2] = xlong_new[:, 2].reshape(ss[0], ss[1])
 
-        # ylong_new = np.transpose(np.matmul(v, np.transpose(xlong_new)))
-        # # Captured Image - Attacked #
-        # y_new = np.zeros(y.shape)
-        # y_new[:, :, 0] = ylong_new[:, 0].reshape(ss[0], ss[1])
-        # y_new[:, :, 1] = ylong_new[:, 1].reshape(ss[0], ss[1])
-        # y_new[:, :, 2] = ylong_new[:, 2].reshape(ss[0], ss[1])
+        ylong_new = np.transpose(np.matmul(v, np.transpose(xlong_new)))
+        # Captured Image - Attacked #
+        y_new = np.zeros(y.shape)
+        y_new[:, :, 0] = ylong_new[:, 0].reshape(ss[0], ss[1])
+        y_new[:, :, 1] = ylong_new[:, 1].reshape(ss[0], ss[1])
+        y_new[:, :, 2] = ylong_new[:, 2].reshape(ss[0], ss[1])
 
         # y = cv2.convertScaleAbs(y, alpha=(255.0))
         # x = cv2.convertScaleAbs(x, alpha=(255.0))
-        # y_new = cv2.convertScaleAbs(y_new, alpha=(255.0))
+        y_new = cv2.convertScaleAbs(y_new, alpha=(255.0))
         x_new = cv2.convertScaleAbs(x_new, alpha=(255.0))
 
+        x_new = cv2.cvtColor(x_new, cv2.COLOR_RGB2BGR)
+        y_new = cv2.cvtColor(y_new, cv2.COLOR_RGB2BGR)
         # Captured original, Projector original, Captured attracked, Projector attacked
-        return x_new
+        return y_new
         # return y, x, y_new, x_new
 
     # , original_filename, actual_label, v_number, n_number):
@@ -87,7 +96,7 @@ class Preprocess():
         """
         # filename = "/home/zhan3447/CS490_DSC/jpg/{actual_label}/v{v_number}/n{n_number}/{original_filename}"
         filename = f"{filepath}.jpg"
-        cv2.imwrite(filename, matrix) # does not work
+        cv2.imwrite(filename, matrix)  # does not work
         # plt.imshow(matrix)
         # plt.savefig(filename)
 
@@ -150,6 +159,7 @@ class Preprocess():
                 V_list.append(V)
                 condition_list.append(eps)
         if identity:
+            # V_list.append(np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]]))
             V_list.append(np.identity(3))
             condition_list.append(-1)
         return V_list, condition_list
