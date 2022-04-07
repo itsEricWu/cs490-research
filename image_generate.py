fe6144d9b773479e91e57c12c7fba5eed0c8a062
luwh2000 <luwh2000@gmail.com>
2022-04-06 20:12:15 -0400

    jpg images deleted

--- image_generate.py
+++ image_generate.py
@@ -5,11 +5,16 @@ import pickle
 from preprocess import Preprocess
 
 
-v, c = Preprocess.generate_constant_v_matrix(10, 1, True)
+v, c = Preprocess.generate_constant_v_matrix(7, 3, True)
 epsilons = [0, 0.05, 0.1, 0.15, 0.2]
 for i, k in zip(v, c):
     for j in epsilons:
-        x_new = Preprocess.preprocess_image("Test/0/32.ppm", i, j)
-        filename = "generated/jpgimages/10_" + str(k) + "_" + str(j)
-        # Preprocess.show_np_array_as_jpg(x_new[:, :, ::-1], filename)
+        x_new = Preprocess.preprocess_image_gaussian("Test/0/32.ppm", i, j)
+        filename = "generated/jpgimages/10_" + str(k) + "_" + str(j) + "_gaussian"
+        Preprocess.show_np_array_as_jpg(x_new, filename)
+        x_new = Preprocess.preprocess_image_speckle("Test/0/32.ppm", i, j)
+        filename = "generated/jpgimages/10_" + str(k) + "_" + str(j) + "_speckle"
+        Preprocess.show_np_array_as_jpg(x_new, filename)
+        x_new = Preprocess.preprocess_image_poisson("Test/0/32.ppm", i, j)
+        filename = "generated/jpgimages/10_" + str(k) + "_" + str(j) + "_poisson"
         Preprocess.show_np_array_as_jpg(x_new, filename)
